eaddc95f76d0fe752949853cff9e08ff02cec807
Suckerman7 <43205411+Suckerman7@users.noreply.github.com>
2023-10-31 23:09:17 +0300

    Create main.dart

---
+++ flut_news/NewsAppTitleLayout/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.brown[100],
+        appBar: AppBar(
+          backgroundColor: Colors.indigo[800],
+          title: Text('News App'),
+        ),
+        body: Center(
+          child: Image.network(
+            'https://upload.wikimedia.org/wikipedia/tr/8/84/Milli_Gazete_26_Aral%C4%B1k_2017.jpg'
+          )
+        )
+      ),
+    ),
+  );
+}
+
