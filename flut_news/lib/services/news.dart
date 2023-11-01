d22e1de18255205cb9bc6fbef99c59ccc9a2e5d2
Muhammet Bozkurt <mubozkt@gmail.com>
2023-11-01 21:59:48 +0300

    Add Networking

---
+++ flut_news/lib/services/news.dart
@@ -0,0 +1,12 @@
+import 'package:flut_news/services/networking.dart';
+import 'package:flut_news/utilities/constants.dart';
+
+
+class NewsModel {
+  Future<dynamic> getNews(String countryName) async {
+    NetworkHelper networkHelper = NetworkHelper(url: '$kNewsApiURL?country=$countryName&apiKey=$kApiKey');
+    var newsData = await networkHelper.getData();
+    return newsData;
+    print(newsData);
+  }
+}
