50882c569cb8a6ae51ab17c4ae10df12447807ef
NeslihanDikmen <139860007+NeslihanDikmen@users.noreply.github.com>
2023-11-08 21:01:29 +0300

    Theme (#7)
    * latest test
    
    * theme
    
    * theme and loading screens
    
    * add screens
    
    ---------
    
    Co-authored-by: Muhammet Bozkurt <mubozkt@gmail.com>

--- flut_news/lib/utilities/constants.dart
+++ flut_news/lib/utilities/constants.dart
@@ -1,4 +1,13 @@
 import 'package:flutter/material.dart';
+bool isDarkMode = false;
 
+final lightTheme = ThemeData(
+  primarySwatch: Colors.yellow,
+);
+final darkTheme = ThemeData(
+  primarySwatch: Colors.teal,
+  brightness: Brightness.dark,
+);
 const kApiKey = '71c117b96d1f46dcba380a95aa14d8cd';
 const kNewsApiURL = 'https://newsapi.org/v2/top-headlines';
+
