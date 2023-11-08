1ff813241872ad4ced281d1d646cb00ab2645395
Muhammet Bozkurt <mubozkt@gmail.com>
2023-11-08 20:56:25 +0300

    add screens

--- flut_news/lib/main.dart
+++ flut_news/lib/main.dart
@@ -20,7 +20,7 @@ class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
-
+// test
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
