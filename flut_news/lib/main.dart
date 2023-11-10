b8b12295921b68cb9c1a2b82d229990a4068c5e6
Suckerman7 <43205411+Suckerman7@users.noreply.github.com>
2023-11-10 15:31:17 +0300

    Location Data Pulling (#9)
    * Location Data pulling is taken care of
    
    took me a couple of hours, but I finally managed to pull it off.
    
    I cannot test it until I push it so... here it is.
    
    * Get Device's Location
    
    
    ---------
    
    After the first PR I managed to successfully change the location_data file to only handle getting location information so it would be suitable for single responsibility.
    I erased the part where it would create a screen for location testing.
    Added a part where we would get access for location permission using android manifest file and adding location package to the pubspec.yaml file 
    
     
    Co-authored-by: Muhammet Bozkurt <mubozkt@gmail.com>

--- flut_news/lib/main.dart
+++ flut_news/lib/main.dart
@@ -1,10 +1,7 @@
-
 import 'package:flut_news/screens/home_screen.dart';
 import 'package:flut_news/screens/loading_screen.dart';
 import 'package:flut_news/screens/theme_test_screen.dart';
-import 'package:flut_news/services/news.dart';
 import 'package:flutter/material.dart';
-
 void main() {
   runApp(MyApp());
 }
@@ -22,7 +19,7 @@ class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
-// test
+
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
--- flut_news/lib/screens/loading_screen.dart
+++ flut_news/lib/screens/loading_screen.dart
@@ -1,19 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:flut_news/services/location.dart';
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
 
   @override
   State<LoadingScreen> createState() => _LoadingScreenState();
 }
-
+Location location = Location();
 class _LoadingScreenState extends State<LoadingScreen> {
   @override
   void initState() {
     super.initState();
+    getLocationData();
+    location.getCurrentLocation();
     Future.delayed(Duration(seconds: 1), () {
       Navigator.pushNamed(context, 'ThemeScreen');
     });  }
+  void getLocationData() async {
+    // var weatherData = await NewsModel().();
+    // Navigator.push(context, MaterialPageRoute(builder: (context) {
+    //   return LocationScreen(
+    //     locationWeather: weatherData,
+    //   );
+    // }));
+
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
