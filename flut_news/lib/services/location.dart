593445392fc7b498935e19a22ca5d40db7c2b958
Muhammet Bozkurt <mubozkt@gmail.com>
2023-11-10 15:26:54 +0300

    Get Device's Location

---
+++ flut_news/lib/services/location.dart
@@ -0,0 +1,30 @@
+import 'package:geolocator/geolocator.dart';
+
+class Location {
+  double latitude = 0;
+  double longitude = 0;
+
+  Future<void> getCurrentLocation() async {
+    await _checkLocationPermission();
+
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.low,
+      );
+      latitude = position.latitude;
+      longitude = position.longitude;
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> _checkLocationPermission() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        print("PERMISSION HAS NOT BEEN GIVEN");
+      }
+    }
+  }
+}
