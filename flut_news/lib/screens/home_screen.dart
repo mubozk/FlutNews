53eb034f30c3a2f9188adc8d43c2e5f22f1a9111
Suckerman7 <43205411+Suckerman7@users.noreply.github.com>
2023-11-14 12:56:31 +0300

    NewsTable (#13)
    * NewsTable Task is done
    
    Sadly I wasn't able to find a way to leave enough space for a navigation bar; but I believe we can find a way.
    
    * Updates N' Fixes
    
    - Changed EdgeInsets.all to EdgeInsets.only
    - Added a piece of code for removing the back button
    
    * update on edgeinsets of individual images
    
    ---------
    
    Co-authored-by: Muhammet Bozkurt <mubozkt@gmail.com>

--- flut_news/lib/screens/home_screen.dart
+++ flut_news/lib/screens/home_screen.dart
@@ -8,10 +8,34 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List images = [
+    "https://images.wallpaperscraft.com/image/single/forest_man_loneliness_116306_1280x960.jpg",
+    "https://images.wallpaperscraft.com/image/single/landscape_mountains_sun_140434_1280x960.jpg",
+    "https://images.wallpaperscraft.com/image/single/ocean_beach_aerial_view_134429_1280x960.jpg",
+    "https://images.wallpaperscraft.com/image/single/lavender_field_starry_sky_123482_1280x960.jpg",
+    "https://images.wallpaperscraft.com/image/single/mountains_lake_tops_119133_1280x960.jpg",
+  ];
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Table Layout"),
+        centerTitle: true,
+        automaticallyImplyLeading: false,
+      ),
+      body: ListView.builder(itemBuilder: (BuildContext ctx, int index){
+        return Padding(
+          padding: EdgeInsets.only(top: 15, left: 10, right: 10, bottom: 5),
+              child: Column(
+                children: <Widget>[
+                  Image.network(images[index]),
+                  SizedBox(height: 20),
+                ],
+              ),
+            );
+      },
+      itemCount: images.length,
+      )
     );
   }
 }
