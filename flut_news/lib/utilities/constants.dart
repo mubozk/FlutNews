c81ca8e0183ad55727b4ad4f2a5517ca2c2601e9
Muhammet Bozkurt <mubozkt@gmail.com>
2023-11-14 16:52:35 +0300

    delete apikey

--- flut_news/lib/utilities/constants.dart
+++ flut_news/lib/utilities/constants.dart
@@ -8,6 +8,6 @@ final darkTheme = ThemeData(
   primarySwatch: Colors.teal,
   brightness: Brightness.dark,
 );
-const kApiKey = '71c117b96d1f46dcba380a95aa14d8cd';
+const kApiKey = '';
 const kNewsApiURL = 'https://newsapi.org/v2/top-headlines';
 
