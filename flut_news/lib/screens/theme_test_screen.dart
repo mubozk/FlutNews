da104135d0e3470763dd92523a7aca109bca2c90
NeslihanDikmen <139860007+NeslihanDikmen@users.noreply.github.com>
2023-11-14 13:18:33 +0300

    haber küpürü (#12)
    * haber küpürü
    
    temayla uyumlu değişebilen haber küpürü
    
    * güncelleme
    
    arka plan görünürlüğü için saydamlık eklendi
    ve tıklanabilir buton görünür hale getirildi
    
    * Touchable ClipNews
    
    ---------
    
    Co-authored-by: Muhammet Bozkurt <mubozkt@gmail.com>

--- flut_news/lib/screens/theme_test_screen.dart
+++ flut_news/lib/screens/theme_test_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flut_news/widgets/clip_news.dart';
 import 'package:flutter/material.dart';
 import 'package:flut_news/utilities/constants.dart';
 
@@ -9,13 +10,12 @@ class ThemeTestScreen extends StatefulWidget {
 }
 
 class _ThemeTestScreenState extends State<ThemeTestScreen> {
-
-
   void toggleTheme() {
     setState(() {
       isDarkMode = !isDarkMode;
     });
   }
+
   @override
   Widget build(BuildContext context) {
     final themeData = isDarkMode ? darkTheme : lightTheme;
@@ -24,14 +24,14 @@ class _ThemeTestScreenState extends State<ThemeTestScreen> {
       theme: themeData,
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Theme Change Example'),
+          title: const Text('Theme Change Example'),
         ),
         body: Container(
           decoration: BoxDecoration(
             image: DecorationImage(
                 image: isDarkMode
-                    ? AssetImage('assets/dark_background.jpg')
-                    : AssetImage('assets/light_background.jpg'),
+                    ? const AssetImage('assets/dark_background.jpg')
+                    : const AssetImage('assets/light_background.jpg'),
                 fit: BoxFit.fill),
           ),
           child: Center(
@@ -39,16 +39,16 @@ class _ThemeTestScreenState extends State<ThemeTestScreen> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 ElevatedButton(
-                  onPressed:
-                  toggleTheme,
+                  onPressed: toggleTheme,
                   child: Text(isDarkMode
                       ? 'Switch to Light Theme'
                       : 'Switch to Dark Theme'),
                 ),
                 Text(
                   'Current Theme: ${isDarkMode ? 'Dark' : 'Light'}',
-                  style: TextStyle(fontSize: 20),
+                  style: const TextStyle(fontSize: 20),
                 ),
+                ClipNews(isDarkMode: isDarkMode),
               ],
             ),
           ),
---
+++ flut_news/lib/widgets/clip_news.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class ClipNews extends StatelessWidget {
+  const ClipNews({
+    super.key,
+    required this.isDarkMode,
+  });
+  final bool isDarkMode;
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        // we'll redirect to the news details screen here when we create it
+      },
+      child: Container(
+        height: 200,
+        width: 400,
+        decoration: BoxDecoration(
+          color: isDarkMode
+              ? Colors.teal.withOpacity(0.7)
+              : Colors.yellow.withOpacity(0.7),
+          borderRadius: const BorderRadius.only(
+            bottomLeft: Radius.circular(10),
+            bottomRight: Radius.circular(10),
+            topLeft: Radius.circular(10),
+            topRight: Radius.circular(15),
+          ),
+          boxShadow: [
+            BoxShadow(
+              offset: const Offset(5, 5),
+              spreadRadius: 5,
+              blurRadius: 10,
+              color: Colors.black.withOpacity(0.5),
+            ),
+          ],
+          gradient: LinearGradient(
+            colors: isDarkMode
+                ? [
+                    const Color.fromARGB(255, 4, 170, 153).withOpacity(0.5),
+                    const Color.fromARGB(255, 3, 190, 171).withOpacity(0.5),
+                    const Color.fromARGB(255, 1, 119, 107).withOpacity(0.5),
+                  ]
+                : [
+                    const Color.fromARGB(255, 238, 222, 81).withOpacity(0.5),
+                    const Color.fromARGB(255, 248, 232, 91).withOpacity(0.5),
+                    const Color.fromARGB(255, 243, 223, 42).withOpacity(0.5)
+                  ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            const Text("başlik"),
+            const Text("body"),
+          ],
+        ),
+      ),
+    );
+  }
+}
