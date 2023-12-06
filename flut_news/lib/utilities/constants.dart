f4c5f20150110f353e5ac48fdc97287a10e186e2
Muhammet Bozkurt <mubozkt@gmail.com>
2023-12-06 21:05:18 +0300

    Refactor clip_news and add new constants

--- flut_news/lib/utilities/constants.dart
+++ flut_news/lib/utilities/constants.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
-bool isDarkMode = false;
+bool kIsDarkMode = false;
 
-final lightTheme = ThemeData(
+final kLightTheme = ThemeData(
   primarySwatch: Colors.yellow,
 );
-final darkTheme = ThemeData(
+final kDarkTheme = ThemeData(
   primarySwatch: Colors.teal,
   brightness: Brightness.dark,
 );
-const kApiKey = '';
+const TextStyle kTitleTextStyle = TextStyle(
+  fontSize: 18,
+  fontWeight: FontWeight.bold,
+);
+const TextStyle kBodyTextStyle = TextStyle(
+  fontSize: 14,
+);
+const TextStyle kReadMoreTextStyle = TextStyle(
+  color: Colors.blue,
+  fontWeight: FontWeight.bold,
+);
+const kApiKey = '71c117b96d1f46dcba380a95aa14d8cd';
 const kNewsApiURL = 'https://newsapi.org/v2/top-headlines';
 
