99737ae360fd03fe691abd962cb22a03e7619235
Suckerman7 <43205411+Suckerman7@users.noreply.github.com>
2023-12-09 15:52:29 +0300

    Update webview_screen.dart
    Biraz daha güncelleme yaptım kodda

--- flut_news/lib/screens/webview_screen.dart
+++ flut_news/lib/screens/webview_screen.dart
@@ -1,31 +1,54 @@
-import 'package:flut_news/utilities/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class WebViewContainer extends StatefulWidget {
-  const WebViewContainer({super.key});
+class WebViewStack extends StatefulWidget {
+  const WebViewStack({super.key});
 
   @override
-  State<WebViewContainer> createState() => _WebViewContainerState();
+  State<WebViewStack> createState() => _WebViewStackState();
 }
 
-class _WebViewContainerState extends State<WebViewContainer> {
-
+class _WebViewStackState extends State<WebViewStack> {
+  var loadingPercentage = 0;
   late final WebViewController controller;
 
   @override
   void initState() {
     super.initState();
     controller = WebViewController()
-    ..setJavaScriptMode(JavaScriptMode.disabled)
-    ..loadRequest(Uri.parse(Uri.dataFromString(kNewsApiURL) as String ));
-
+    ..setNavigationDelegate(NavigationDelegate(
+      onPageStarted: (url) {
+        setState(() {
+          loadingPercentage = 0;
+        });
+      },
+      onProgress: (progress) {
+        setState(() {
+          loadingPercentage = progress;
+        });
+      },
+      onPageFinished: (url) {
+        setState(() {
+          loadingPercentage = 100;
+        });
+      },
+    ))
+      ..loadRequest(
+        Uri.parse('https://flutter.dev'),
+      );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: WebViewWidget(controller: controller),
+    return Stack(
+      children: [
+        WebViewWidget(controller: controller,
+        ),
+        if (loadingPercentage < 100)
+        LinearProgressIndicator(
+          value: loadingPercentage / 100.0,
+        ),
+      ],
     );
   }
 }
