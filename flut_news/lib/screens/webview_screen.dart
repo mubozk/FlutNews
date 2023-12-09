47eaae271df1ac5e36fcbdc4c49ca98d004c49cd
Suckerman7 <43205411+Suckerman7@users.noreply.github.com>
2023-12-09 20:16:29 +0300

    Update webview_screen.dart
    Gelebildiğim kadar geldim ileriye bugün.
    
    Yarın belki göz atabiliriz.

--- flut_news/lib/screens/webview_screen.dart
+++ flut_news/lib/screens/webview_screen.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class WebViewStack extends StatefulWidget {
-  const WebViewStack({super.key});
+  const WebViewStack({required this.controller, super.key});
+
+  final WebViewController controller;
 
   @override
   State<WebViewStack> createState() => _WebViewStackState();
@@ -10,39 +12,52 @@ class WebViewStack extends StatefulWidget {
 
 class _WebViewStackState extends State<WebViewStack> {
   var loadingPercentage = 0;
-  late final WebViewController controller;
 
   @override
   void initState() {
     super.initState();
-    controller = WebViewController()
-    ..setNavigationDelegate(NavigationDelegate(
-      onPageStarted: (url) {
-        setState(() {
-          loadingPercentage = 0;
-        });
-      },
-      onProgress: (progress) {
-        setState(() {
-          loadingPercentage = progress;
-        });
-      },
-      onPageFinished: (url) {
-        setState(() {
-          loadingPercentage = 100;
-        });
-      },
-    ))
-      ..loadRequest(
-        Uri.parse('https://flutter.dev'),
-      );
+    widget.controller.setNavigationDelegate(
+      NavigationDelegate(
+        onPageStarted: (url) {
+          setState(() {
+            loadingPercentage = 0;
+          });
+        },
+        onProgress: (progress) {
+          setState(() {
+            loadingPercentage = progress;
+          });
+        },
+        onPageFinished: (url) {
+          setState(() {
+            loadingPercentage = 100;
+          });
+        },
+
+        onNavigationRequest: (navigation) {
+          final host = Uri.parse(navigation.url).host;
+          if (host.contains('youtube.com')) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text(
+                  'Blocking navigation to $host',
+                ),
+              ),
+            );
+            return NavigationDecision.prevent;
+          }
+          return NavigationDecision.navigate;
+        }
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        WebViewWidget(controller: controller,
+        WebViewWidget(
+          controller: widget.controller,
         ),
         if (loadingPercentage < 100)
         LinearProgressIndicator(
