e3c42af23ce88127462c3c7b4cfc6bb69194360f
Muhammet Bozkurt <mubozkt@gmail.com>
2023-12-19 03:51:23 +0300

    hotfix clip_news theme

--- flut_news/lib/components/clip_news.dart
+++ flut_news/lib/components/clip_news.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flut_news/utilities/constants.dart';
 import 'package:flut_news/screens/news_webview_screen.dart';
+
 class ClipNews extends StatelessWidget {
   final String title;
   final String body;
@@ -15,73 +16,91 @@ class ClipNews extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 200,
-      margin: const EdgeInsets.all(8.0),
-      decoration: BoxDecoration(
-        color: kIsDarkMode ? Colors.teal.withOpacity(0.7) : Colors.yellow.withOpacity(0.7),
-        borderRadius: BorderRadius.circular(15),
-        boxShadow: [
-          BoxShadow(
-            offset: Offset(0, 4),
-            blurRadius: 8,
-            color: Colors.black.withOpacity(0.2),
-          ),
-        ],
-        gradient: LinearGradient(
-          colors: kIsDarkMode
-              ? [Colors.teal.shade700, Colors.teal.shade500, Colors.teal.shade300]
-              : [Colors.yellow.shade800, Colors.yellow.shade600, Colors.yellow.shade400],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Text(
-              title,
-              style: kTitleTextStyle.copyWith(
-                color: kIsDarkMode ? Colors.white : Colors.black,
-              ),
-              overflow: TextOverflow.ellipsis,
-            ),
-            SizedBox(height: 8),
-            Expanded(
-              child: Text(
-                body,
-                style: kBodyTextStyle.copyWith(
-                  color: kIsDarkMode ? Colors.white.withOpacity(0.9) : Colors.black.withOpacity(0.9),
+    return ValueListenableBuilder(
+        valueListenable: kIsDarkModeNotifier,
+        builder: (context, bool isDarkMode, _) {
+          return Container(
+            height: 200,
+            margin: const EdgeInsets.all(8.0),
+            decoration: BoxDecoration(
+              color: isDarkMode
+                  ? Colors.teal.withOpacity(0.7)
+                  : Colors.yellow.withOpacity(0.7),
+              borderRadius: BorderRadius.circular(15),
+              boxShadow: [
+                BoxShadow(
+                  offset: Offset(0, 4),
+                  blurRadius: 8,
+                  color: Colors.black.withOpacity(0.2),
                 ),
-                overflow: TextOverflow.ellipsis,
-                maxLines: 5,
+              ],
+              gradient: LinearGradient(
+                colors: isDarkMode
+                    ? [
+                        Colors.teal.shade700,
+                        Colors.teal.shade500,
+                        Colors.teal.shade300
+                      ]
+                    : [
+                        Colors.yellow.shade800,
+                        Colors.yellow.shade600,
+                        Colors.yellow.shade400
+                      ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
               ),
             ),
-            Align(
-              alignment: Alignment.bottomRight,
-              child: TextButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => NewsWebViewScreen(url: url),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(
+                    title,
+                    style: kTitleTextStyle.copyWith(
+                      color: isDarkMode ? Colors.white : Colors.black,
                     ),
-                  );
-                },
-                child: Text(
-                  'Read More',
-                  style: kReadMoreTextStyle.copyWith(
-                    color: kIsDarkMode ? Colors.lightBlueAccent : Colors.blue,
+                    overflow: TextOverflow.ellipsis,
                   ),
-                ),
+                  SizedBox(height: 8),
+                  Expanded(
+                    child: Text(
+                      body,
+                      style: kBodyTextStyle.copyWith(
+                        color: isDarkMode
+                            ? Colors.white.withOpacity(0.9)
+                            : Colors.black.withOpacity(0.9),
+                      ),
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 5,
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.bottomRight,
+                    child: TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => NewsWebViewScreen(url: url),
+                          ),
+                        );
+                      },
+                      child: Text(
+                        'Read More',
+                        style: kReadMoreTextStyle.copyWith(
+                          color: isDarkMode
+                              ? Colors.lightBlueAccent
+                              : Colors.blue,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
-      ),
-    );
+          );
+        });
   }
 }
